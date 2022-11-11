c199215ec939c9ca43a4adacc47923d813c7ea28
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2022-11-11 11:21:53 +0900

    main page 추가

--- default-setup/src/App.tsx
+++ default-setup/src/App.tsx
@@ -2,13 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Layout from './components/layouts/Layout';
-
+import MainPage from './pages/MainPage';
 
 const App = () => {
     return (
         <Wrapper>
             <Routes>
-                {/* <Route path="/" element={<Layout><TodoListPage /></Layout>} /> */}
+                <Route path="/" element={<Layout><MainPage /></Layout>} />
             </Routes>
         </Wrapper>
     );
---
+++ default-setup/src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MainPage = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+const Wrapper = styled.div``;
+
+export default MainPage;
