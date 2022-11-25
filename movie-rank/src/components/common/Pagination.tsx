086368308ca66e9e1cdc92feddb499d7f9081117
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2022-11-25 11:39:50 +0900

    페이지네이션 구현 완료

--- movie-rank/src/components/common/Pagination.tsx
+++ movie-rank/src/components/common/Pagination.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
 
 interface PaginationProps {
     total: number,
@@ -10,9 +11,24 @@ interface PaginationProps {
 
 const Pagination = ({total, limit, page, setPage, onClick}:PaginationProps) => {
     const numPages = Array.from({length: Math.ceil(Number(total)/limit)}, (_, i) => i + 1)
-    
+    const [currentPage, setCurrentPage] = useState<number>(1);
+
     const handleSetPage = (number: number) => {
+        if(number >= currentPage){
+            onClick(number + 1);
+            setCurrentPage(number + 1);
+        }else if(number <= currentPage){
+            onClick(number + 5);
+            setCurrentPage(number + 5);
+        }
         setPage(number);
+    }
+
+    const handleOnClick = (item: number) => {
+        if(currentPage !== item){
+            onClick(item);
+            setCurrentPage(item);
+        }
     }
 
     return (
@@ -26,7 +42,8 @@ const Pagination = ({total, limit, page, setPage, onClick}:PaginationProps) => {
             {numPages.slice(page, page + 5).map((item) => (
                 <Button
                     key={`pagenation${item}`}
-                    onClick={() => onClick(item)}
+                    onClick={() => handleOnClick(item)}
+                    current={currentPage === item}
                 >
                     {item}
                 </Button>
@@ -43,6 +60,20 @@ const Pagination = ({total, limit, page, setPage, onClick}:PaginationProps) => {
 const Wrapper = styled.div`
     display: flex;
 `;
-const Button = styled.button``;
+const Button = styled.button<{current?: boolean, disabled?: boolean}>`
+    background-color: transparent;
+    color: white;
+    border: none;
+    
+    font-size: 18px;
+
+    ${({disabled}) => disabled && css`
+        color: transparent;
+    `}
+
+    ${({current}) => current && css`
+        color: red;
+    `}
+`;
 
 export default Pagination;
