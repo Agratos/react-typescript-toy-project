699b0e3b3d86739cd00d727d093c196d89c0213b
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2022-12-07 14:57:47 +0900

    시간 설정 완료 / 무한 루프는 아직 미구현

--- alarm/src/components/alarm/AlarmRegister.tsx
+++ alarm/src/components/alarm/AlarmRegister.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, {useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 import AlarmHeader from './AlarmHeader';
@@ -6,33 +6,75 @@ import RepeatDay from './components/RepeatDay';
 
 import alarmStore, { IAlarmState} from 'src/modules/zustand/alarm';
 
+interface IHTMLDivElement extends HTMLDivElement{
+    [index:string]: any
+}
+
 const AlarmRegister = () => {
     const [alarmDay, setAlarmDay] = useState<any>({월: false,화: false,수: false,목: false,금: false,토: false,일: true});
+    const minuteRef = useRef<IHTMLDivElement>(null);
+    const hourRef = useRef<IHTMLDivElement>(null);
 
     const minuteList = Array(60).fill(0).map((arr, index) => {return index});
     const hourList = Array(12).fill(0).map((arr, index) => {
-        if(index > 9){
+        if(index > 8){
             return `${index + 1}`
         }
         return `0${index + 1}`
     });
+
+    useEffect(() => {
+        hourRef.current!.hour = 6;
+    },[])
+
+    const handleMouseDown = (ref:React.RefObject<IHTMLDivElement>, position:number) => {
+        const target = ref.current!;
+        target.isClick = true;
+        target.startPosition = position;
+    }
+    const handleMouseMove = (ref:React.RefObject<IHTMLDivElement>, position:number) => {
+        const target = ref.current!;
+        if(target.isClick){
+            if(target.startPosition - position >= 60){
+                target.hour = target.hour - 1;
+                target.style.transition = `all 0s ease-in-out`;
+                target.style.transform = `translateY(${60 * (target.hour - 6)}px)`;
+                target.startPosition = position;
+            }else if(target.startPosition - position <= -60){
+                target.hour = target.hour + 1;
+                target.style.transition = `all 0s ease-in-out`;
+                target.style.transform = `translateY(${60 * (target.hour - 6)}px)`;
+                target.startPosition = position;
+            }
+        }
+    }
+    const handleMouseUp = (ref:React.RefObject<IHTMLDivElement>) => {
+        ref.current!.isClick = false
+
+    }  
 
     return (
         <Wrapper>
             <AlarmHeader text={'Choose a time to wake up'} />
             <TimeChooseWrapper>
                 <TimeChoose>
-                    <HourSelect>
+                    <HourSelect 
+                        ref={hourRef}
+                        onMouseDown={(e) => handleMouseDown(hourRef, e.clientY)}
+                        onMouseMove={(e) => handleMouseMove(hourRef, e.clientY)}
+                        onMouseUp={() => handleMouseUp(hourRef)}
+                        onMouseLeave={() => handleMouseUp(hourRef)}
+                    >
                         {hourList.map((hour) => (
-                            <div>{hour}</div>
+                            <div key={`hour-${hour}`}>{hour}</div>
                         ))}
                     </HourSelect>
                     <Colon>:</Colon>
-                    <TimeSelect>
+                    <MinuteSelect ref={minuteRef}>
                         {minuteList.map((minute) => (
-                            <div>{minute}</div>
+                            <div key={`minute-${minute}`}>{minute}</div>
                         ))}
-                    </TimeSelect>
+                    </MinuteSelect>
                 </TimeChoose>
             </TimeChooseWrapper>
             <RepeatDayWrapper>
@@ -47,7 +89,9 @@ const AlarmRegister = () => {
                 <MessageInput />
                 <MessageLabel>What's this for?</MessageLabel>
             </MessageInputWrapper>
-            <RegisterButton>
+            <RegisterButton
+                onClick={() => console.log(`설정한 시각은 ${12 - hourRef.current!.hour} : 30 분 입니다.`)}
+            >
                 ALL SET
             </RegisterButton>
         </Wrapper>
@@ -73,26 +117,32 @@ const TimeChooseWrapper = styled.div`
     justify-content: center;
     flex:1;
     width: 100%;
-    height: 264px;
     align-items: center;
     text-align: center;
+    margin: 20px 0;
+    overflow: hidden;
 `;
 const TimeChoose = styled.div`
     display: flex;
     justify-content: space-around;
     width: 50%;
+`;
+const Colon = styled.div`
+    ${({theme}) => theme.div.vhCenter}
+    font-size: 40px;
     height: inherit;
 `;
 const HourSelect = styled.div`
-    //${({theme}) => theme.div.vhCenter}
-    display: flex;
-    flex-direction: column;
+    ${({theme}) => theme.div.vhCenter}
     font-size: 48px;
     height: inherit;
     overflow-y: hidden;
+    padding-top: 60px;
+    cursor: pointer;
 `;
-const Colon = styled(HourSelect)``;
-const TimeSelect = styled(HourSelect)``;
+const MinuteSelect = styled(HourSelect)`
+    padding-bottom: 120px;
+`;
 const RepeatDayWrapper = styled.div``;
 const MessageInputWrapper = styled.div`
     display: flex;
